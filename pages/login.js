abad4507d146b3a3ecadf4d2eea9e7ae91642816
blvckcenturion <mr.plus.co@gmail.com>
2021-11-07 12:43:24 -0400

    Finished styling for Login page

--- pages/login.js
+++ pages/login.js
@@ -11,22 +11,18 @@ const Login = () => {
     return (
         <div className="login-page">
             <div className="login-page-card">
-                <div className="login-page-card-header">
-                    <h1>Inicia Sesion.</h1>
-                </div>
-                <div className="login-page-card-body">
-                    <form>
-                        <div className="login-form-input">
-                            <FontAwesomeIcon icon={faUser} />
-                            <input type="mail" id="usermail" required placeholder="Correo Electronico"  onChange={(e) => setMail(e.target.value)} value={ mail }/>
-                        </div>
-                        <div className="form-group">
-                            <FontAwesomeIcon icon={faUnlockAlt} />
-                            <input type="password" id="query" required placeholder="Contraseña"  onChange={(e) => setPassword(e.target.value)} value={ password }/>
-                        </div>
-                        <button>Iniciar Sesion</button>
-                    </form>
-                </div>
+                <h1>Inicia Sesion</h1>
+                <form>
+                    <div className="login-form-input">
+                        <FontAwesomeIcon icon={faUser} />
+                        <input type="mail" id="usermail" required placeholder="Correo Electronico"  onChange={(e) => setMail(e.target.value)} value={ mail }/>
+                    </div>
+                    <div className="form-group">
+                        <FontAwesomeIcon icon={faUnlockAlt} />
+                        <input type="password" id="query" required placeholder="Contraseña"  onChange={(e) => setPassword(e.target.value)} value={ password }/>
+                    </div>
+                    <button>Iniciar Sesion</button>
+                </form>
             </div>
         </div>
     )
