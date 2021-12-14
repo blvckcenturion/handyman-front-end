6616fd9dd7f47422744b787f9e34e7a4f285d956
blvckcenturion <mr.plus.co@gmail.com>
2021-12-13 22:24:08 -0400

    Minor updates

--- pages/myprofile.js
+++ pages/myprofile.js
@@ -1,15 +1,75 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUser, faHandSpock,  } from "@fortawesome/free-regular-svg-icons"
+import { faCameraRetro } from "@fortawesome/free-solid-svg-icons"
 
 const Myprofile = () => {
+    
     return (
+        <>
         <div className="myprofile-page">
             <h1>MI PERFIL</h1>
             <div>
-
+                <div>
+                    <FontAwesomeIcon icon={faUser} className="icon" />
+                    <h3>Editar Perfil.</h3>
+                    <Overlay isOpen={false}>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputEmail1">Nombre</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputPassword1">Apellido</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Apellido" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputPassword1">Fecha de Nacimiento</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Fecha de Nacimiento" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputPassword1">Email</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Email" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputPassword1">Contraseña</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Contraseña" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputPassword1">Confirmar Contraseña</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Confirmar Contraseña" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputPassword1">Foto de Perfil</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Foto de Perfil" />
+                        </div>
+                    </Overlay>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faHandSpock} className="icon" />
+                    <h3>Opiniones Sobre Mi.</h3>
+                    
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faCameraRetro} className="icon" />
+                    <h3>Subir Fotografias.</h3>
+                </div>
             </div>
         </div>
+        <Overlay isOpen={true}>
+            <h1>Hola</h1>
+        </Overlay>
+        </>
     )
 }
 
+const Overlay = ({children, isOpen}) => {
+    return isOpen ? (
+        <div className="overlay">
+            <div className="overlay-content">
+                {children}
+            </div>
+        </div>
+    ) : null
+}
 
 export default Myprofile
--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,65 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faKey } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
+import { AVAILABLE_CITIES, CATEGORIES } from '../utils/dummyData'
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className="register-page">
+            <form className="register-form" action="">
+                
+                <h1>REGISTRATE</h1>
+                    <div className="register-form-input">
+                        <FontAwesomeIcon icon={faUser} />
+                        <input type="mail" id="usermail" placeholder="Correo electronico" required/>
+                    </div>
+                    <div className="register-form-input">
+                        <FontAwesomeIcon icon={faKey} />
+                        <input type="password" id="userpassword" placeholder="Contraseña" required />
+                    </div>
+                    <div className="register-form-input">
+                        <FontAwesomeIcon icon={faKey} />
+                        <input type="password" id="userrepeatpassword" placeholder="Repetir Contraseña" required />
+                    </div>
+                    <h2>CUENTANOS SOBRE VOS</h2>
+                    <div className="register-form-input-section-2">
+                        <label htmlFor="userdesc">Breve descripcion personal</label>
+                        <input type="text" id="userdesc" required/>
+                    </div>
+                    <div className="register-form-input-section-2">
+                        <label htmlFor="userpricing">¿Cuanto sueles cobrar por hora? (BOB)</label>
+                        <input type="number" id="userpricing"  required />
+                    </div>
+                    <div className="register-form-input-section-2">
+                        <label htmlFor="usercategory">Categoria en la que te desempeñas</label>
+                        <select name="category" id="usercategory" required>
+                            {CATEGORIES.map((category, i) => <option key={i} value={i}>{category.name}</option>)}
+                        </select>
+                    </div>
+                    <div className="register-form-input-section-2">
+                        <label htmlFor="userworkdesc">Describenos que servicios ofreces</label>
+                        <input type="text" id="userworkdesc" required/>
+                    </div>
+                    <div className="register-form-input-section-2">
+                        <label htmlFor="userphone">Numero de telefono</label>
+                        <input type="number" id="userphone" required/>
+                    </div>
+                    <div className="register-form-input-section-2">
+                    <label htmlFor="usercity">Ciudad</label>
+                        <select name="category" id="usercity" required>
+                            {AVAILABLE_CITIES.map((city, i) => <option key={i} value={city}>{city}</option>)}
+                        </select>
+                    </div>
+                    <div className="register-form-input-section-2">
+                        <label htmlFor="userpic">Foto de perfil</label>
+                        <input type="text" id="url" required/>
+                    </div>
+                    <button>
+                        Registrarse
+                    </button>
+                
+            </form>
         </div>
     )
 }
